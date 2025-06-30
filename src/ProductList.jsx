d225e1b4d8e5915f40e4ec172001f0ee191876c8
quentin kinard <quentinskinard@gmail.com>
2025-06-30 08:00:52 -0400

    made changes to the ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -11,10 +10,6 @@ function ProductList({ onHomeClick }) {
 
     const dispatch = useDispatch()
     const CartItems = useSelector(state => state.cart.items)
-
-    const calculateTotalQuantity = () => {
-        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
-    };
 
     const plantsArray = [
         {
@@ -243,6 +238,10 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
+
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
 
     const handleHomeClick = (e) => {
         e.preventDefault();
