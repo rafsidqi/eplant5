433526ab52e77394efd9121dcf8ebff9d7c0254c
quentin kinard <quentinskinard@gmail.com>
2025-06-30 07:51:18 -0400

    added use dispacth and use selector

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState();
+    const [addedToCart, setAddedToCart] = useState({});
 
     const dispatch = useDispatch()
     const CartItems = useSelector(state => state.cart.items)
